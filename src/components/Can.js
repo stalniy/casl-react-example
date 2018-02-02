2dd156e1923b396139880b448aeb662f3d68ea2b
Sergey Stotskiy <sergey.stotskiy@namecheap.com>
2018-02-02 12:01:33 +0200

    refactor(can): utilizes new update event

--- src/components/Can.js
+++ src/components/Can.js
@@ -2,8 +2,7 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import ability from '../config/ability';
 
-const WARDENS = [];
-let isAbilityPatched;
+const noop = () => {};
 
 export default class Can extends PureComponent {
   static propTypes = {
@@ -11,48 +10,35 @@ export default class Can extends PureComponent {
     on: PropTypes.oneOfType([PropTypes.object, PropTypes.string])
   };
 
+  unsubscribeFromAbility = noop;
   state = {
     allowed: false
-  }
+  };
 
   get allowed() {
-    return this.state.allowed
+    return this.state.allowed;
   }
 
   componentWillMount() {
-    WARDENS.push(this)
-
-    if (!isAbilityPatched) {
-      this.makeAbilitiesToBeReactive();
-    }
-
-    this.recheck()
+    this.unsubscribeFromAbility = ability.on('update', () => {
+      setTimeout(() => this.recheck(), 0)
+    });
+    this.recheck();
   }
 
   componentWillUnmount() {
-    const index = WARDENS.indexOf(this)
-    WARDENS.splice(index, 1)
-  }
-
-  makeAbilitiesToBeReactive() {
-    const updateAbilities = ability.update;
-
-    ability.update = (...args) => {
-      const result = updateAbilities.apply(ability, args);
-      WARDENS.forEach(instance => instance.recheck());
-      return result;
-    }
+    this.unsubscribeFromAbility();
   }
 
   recheck() {
-    return this.setState({ allowed: this.check() })
+    return this.setState({ allowed: this.check() });
   }
 
   check() {
-    return ability.can(this.props.run, this.props.on)
+    return ability.can(this.props.run, this.props.on);
   }
 
   render() {
-    return this.state.allowed ? (<div>{this.props.children}</div>) : '';
+    return this.state.allowed ? React.Children.only(this.props.children) : '';
   }
 }
